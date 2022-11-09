a0c13e8c88d6dea8797bee2a0dce5ad6f4995b64
Marc PEREZ <marc.perez.pro@gmail.com>
2022-11-09 10:17:12 +0100

    Code is working

--- auto-fastlane-spaceauth/app.rb
+++ auto-fastlane-spaceauth/app.rb
@@ -1,21 +1,29 @@
-require 'json'
+require "json"
 require "pty"
 require "expect"
 require "fastlane"
 require "spaceship"
 require "aws-sdk"
 require "aws-sdk-sqs"
-require 'pry' # TODO - remove this line
+require "aws-sdk-secretsmanager"
 
+# In the environment we need:
+# SQS_QUEUE_URL - URL of the SQS queue to receive SMS messages from Apple
+# SPACESHIP_2FA_SMS_DEFAULT_PHONE_NUMBER - Phone number to receive 2FA code (the AWS Pinpoint phone number)
+# FASTLANE_USER - Apple ID
+# FASTLANE_PASSWORD - Apple ID password
+# SESSION_MANAGER_SECRET_ID - AWS Secrets Manager secret ID to store the FASTLANE_SESSION
 
 # Borrowed from https://blog.kishikawakatsumi.com/entry/2021/01/28/074522
 # Modified to work with AWS
-def fastlane_spaceauth()
+def fastlane_spaceauth(fastlane_session = "")
   $expect_verbose = true
 
-  sqs = Aws::SQS::Client.new
-
   # Run the `fastlane spaceauth` command using PTY to respond to 2FA input
+  unless fastlane_session.empty?
+    ENV["FASTLANE_SESSION"] = fastlane_session
+  end
+  ENV["FASTLANE_DISABLE_COLORS"] = "1" # Clean up the logs
   cmd = "bundle exec fastlane spaceauth"
   PTY.spawn(cmd) do |r, w|
     w.sync = true
@@ -32,6 +40,7 @@ def fastlane_spaceauth()
       raise "UnknownError" unless match
 
       # Retrieve SMS containing 2FA code from the API
+      sqs = Aws::SQS::Client.new
       messages = sqs.receive_message({
         queue_url: ENV["SQS_QUEUE_URL"],
         max_number_of_messages: 1,
@@ -62,35 +71,52 @@ def fastlane_spaceauth()
     end
 
     r.expect(/FASTLANE_SESSION='(.+)'/, 10) do |match|
-      binding.pry
       raise "UnknownError" unless match
-      binding.pry
       return match[1]
     end
 
   end
 end
 
-# TODO: Activate this
-# def lambda_handler(event:, context:)
-#   session = fastlane_spaceauth()
-# # TODO Send session to secrets manager
+# Gets the FASTLANE_SESSION from AWS Secrets Manager.
+def get_previously_saved_session()
+  client = Aws::SecretsManager::Client.new()
+  resp = client.get_secret_value({
+    secret_id: ENV["SESSION_MANAGER_SECRET_ID"]
+  })
+  secrets = JSON.parse(resp.secret_string)
+  session = secrets['FASTLANE_SESSION']
+  puts "Successfully got the FASTLANE_SESSION from AWS Secrets Manager."
+  return session
+end
 
-#   {
-#     statusCode: 200,
-#     body: {
-#       message: "Hello World!",
-#       # location: response.body
-#     }.to_json
-#   }
-# end
+# Updates the FASTLANE_SESSION in AWS Secrets Manager.
+def save_session(fastlane_session = "")
+  client = Aws::SecretsManager::Client.new()
+  resp = client.get_secret_value({
+    secret_id: ENV["SESSION_MANAGER_SECRET_ID"]
+  })
+  secrets = JSON.parse(resp.secret_string)
+  secrets['FASTLANE_SESSION'] = fastlane_session
+  client.put_secret_value({
+    secret_id: resp.arn,
+    secret_string:  JSON.generate(secrets)
+  })
+  puts "Successfully set the FASTLANE_SESSION in AWS Secrets Manager."
+end
 
-# TODO remove this
-begin
-  session = fastlane_spaceauth()
-  puts session
-  binding.pry
-rescue => e
-  puts e
-  binding.pry
+def lambda_handler(event:, context:)
+  previous_fastlane_session = get_previously_saved_session()
+  new_fastlane_session = fastlane_spaceauth(previous_fastlane_session)
+  puts # Add a new line to the output for readability
+  if new_fastlane_session != previous_fastlane_session
+    save_session(new_fastlane_session)
+    puts "Session updated"
+  else
+    puts "Session is still valid"
+  end
+
+  return {
+      FASTLANE_SESSION: new_fastlane_session,
+  }
 end
