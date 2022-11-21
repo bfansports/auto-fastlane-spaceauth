3b40f58f101a0ecc13d1b51c3715864035ad8ebe
Marc PEREZ <marc.perez.pro@gmail.com>
2022-11-21 16:16:51 +0100

    WiP on something crazy cool

--- auto-fastlane-spaceauth/app.rb
+++ auto-fastlane-spaceauth/app.rb
@@ -1,6 +1,4 @@
 require "json"
-require "pty"
-require "expect"
 require "fastlane"
 require "spaceship"
 require "aws-sdk-sqs"
@@ -13,68 +11,57 @@ require "aws-sdk-secretsmanager"
 # FASTLANE_PASSWORD - Apple ID password for Fastlane
 # SECRETS_MANAGER_SECRET_ID - AWS Secrets Manager secret ID to store the FASTLANE_SESSION
 
-# Borrowed from https://blog.kishikawakatsumi.com/entry/2021/01/28/074522
-# Modified to work with AWS
-def fastlane_spaceauth(fastlane_session = "")
-  $expect_verbose = true
+# Overrides spaceship/lib/spaceship/two_step_or_factor_client.rb
+class 2FAInterceptorClient
+  def ask_for_2fa_code
+    puts "overrided"
+    # Retrieve SMS containing 2FA code from the API
+    sqs = Aws::SQS::Client.new
+    messages = sqs.receive_message({
+      queue_url: ENV["SQS_QUEUE_URL"],
+      max_number_of_messages: 1,
+      wait_time_seconds: 20,
+    })
+    if messages.messages.empty?
+      raise "NotFoundError"
+    end
+    message = messages.messages[0]
+    message_body = JSON.parse(JSON.parse(message.body)["Message"])["messageBody"]
 
+    puts message_body
+
+    # Parse a 2FA code from the SMS body
+    code = message_body[/\d{6}/]
+    if code.nil? || code.empty?
+      raise "NotFoundError"
+    end
+
+    puts code
+
+    # Cleanup the SQS queue
+    sqs.delete_message({
+      queue_url: ENV["SQS_QUEUE_URL"],
+      receipt_handle: message.receipt_handle,
+    })
+
+    return code
+  end
+end
+
+def fastlane_spaceauth(fastlane_session = "")
   # Run the `fastlane spaceauth` command using PTY to respond to 2FA input
+  # Set the previous session in case it's still valid
   unless fastlane_session.empty?
     ENV["FASTLANE_SESSION"] = fastlane_session
   end
-  ENV["FASTLANE_DISABLE_COLORS"] = "1" # Clean up the logs
-  cmd = "bundle exec fastlane spaceauth"
-  PTY.spawn(cmd) do |r, w|
-    w.sync = true
-
-    # If there is a valid session cookie, return it
-    r.expect(/FASTLANE_SESSION='(.+)'/, 10) do |match|
-      if match
-        return match[1]
-      end
-    end
-
-    # If the session is invalid and need to enter the 2FA code
-    r.expect(/Please enter the 6 digit code you received at .+:/, 60) do |match|
-      raise "UnknownError" unless match
-
-      # Retrieve SMS containing 2FA code from the API
-      sqs = Aws::SQS::Client.new
-      messages = sqs.receive_message({
-        queue_url: ENV["SQS_QUEUE_URL"],
-        max_number_of_messages: 1,
-        wait_time_seconds: 20,
-      })
-      if messages.messages.empty?
-        raise "NotFoundError"
-      end
-      message = messages.messages[0]
-      message_body = JSON.parse(JSON.parse(message.body)["Message"])["messageBody"]
-
-      puts message_body
-
-      # Parse a 2FA code from the SMS body
-      code = message_body[/\d{6}/]
-      if code.nil? || code.empty?
-        raise "NotFoundError"
-      end
-
-      # Enter the code
-      w.puts code
-
-      # Cleanup the SQS queue
-      sqs.delete_message({
-        queue_url: ENV["SQS_QUEUE_URL"],
-        receipt_handle: message.receipt_handle,
-      })
-    end
-
-    r.expect(/FASTLANE_SESSION='(.+)'/, 10) do |match|
-      raise "UnknownError" unless match
-      return match[1]
-    end
-
-  end
+  # Clean up the logs
+  ENV["FASTLANE_DISABLE_COLORS"] = "1"
+  # Dark Magic
+  Spaceship.Client.singleton_class.prepend(2FAInterceptorClient)
+  # Let's go !
+  fastlane_session = Spaceship::SpaceauthRunner.new(copy_to_clipboard: false).run.session_string
+  # Done
+  return fastlane_session
 end
 
 # Gets the FASTLANE_SESSION from AWS Secrets Manager.
